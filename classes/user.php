a0927054aa2ca7fa57a39518642a531d106acd0d
francesca <fradeluca85@gmail.com>
2022-01-31 16:49:21 +0100

    public classes

--- classes/user.php
+++ classes/user.php
@@ -6,12 +6,26 @@ class User
     private $cardNumber;
     private $birthday;
     private $address;
-    private $phoneNumber;
+    private $email;
 
-    public function __construct($_name, $_lastname, $_cardNumber)
+    public function __construct($_name, $_lastname, $_email)
     {
         $this->name = $_name;
         $this->lastname = $_lastname;
-        $this->cardNumber = $_cardNumber;
+        $this->cardNumber = $_email;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getlastName()
+    {
+        return $this->lastname;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
     }
 }
